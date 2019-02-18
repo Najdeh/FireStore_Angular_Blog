888587a6f8c2debad612ec2edce0e3d22649f52b
Miro <m.najdeh@gmail.com>
2019-02-18 06:24:41 +0100

    post detail component implemented

--- src/app/posts/post-detail/post-detail.component.ts
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PostService } from '../post.service';
 import { Post } from '../post';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -10,7 +11,7 @@ import { Post } from '../post';
 })
 export class PostDetailComponent implements OnInit {
 post: Post
-  constructor(private route: ActivatedRoute, private postService: PostService) { }
+  constructor(private route: ActivatedRoute, private postService: PostService, private router: Router, authService: AuthService) { }
 
   ngOnInit() {
     this.getPost()
@@ -19,5 +20,11 @@ post: Post
   getPost() {
     const id = this.route.snapshot.paramMap.get('id')
     return this.postService.getPostData(id).subscribe(data => this.post = data)
+  }
+
+  delete() {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.postService.delete(id)
+    this.router.navigate(["/blog"])
   }
 }
